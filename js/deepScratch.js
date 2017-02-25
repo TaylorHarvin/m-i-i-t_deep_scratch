c08675062cdd257c5b92e0ffa87c2e6827077159
Taylor Harvin <tharvin12@gmail.com>
2017-02-24 23:03:41 -0500

    Added json pull for object template, added add variable method and show method params; added general object layout.

--- js/deepScratch.js
+++ js/deepScratch.js
@@ -2,7 +2,48 @@ var deepScratchApp = angular.module('deepScratchApp',[]);
 
 
 deepScratchApp.controller("objectCreator",function($scope, $rootScope,$http){
-	$scope.objects = [{"vars":{"type":"int","name":"width","value":"5"}}];
+	$scope.objects = [
+		{"className":"Shape",
+			"vars":[
+				{"type":"int","name":"width","value":"5"}
+			],
+			"showParams":false,
+			"methods":[
+				{"return":"void","name":"setWidth","params":[
+					{"type":"int","name":"width","value":"5"}
+					]
+				}
+			]
+		},
+		{"className":"Circle",
+			"vars":[
+				{"type":"int","name":"width","value":"5"}
+			],
+			"showParams":false,
+			"methods":[
+				{"return":"void","name":"setRadius","params":[
+					{"type":"int","name":"radius","value":"5"},
+					{"type":"string","name":"units","value":"cm"}
+					]
+				}
+			]
+		}
+	];
+
+	$scope.setShowParams = function(classIndex){
+		$scope.objects[classIndex].showParams = !$scope.objects[classIndex].showParams;
+	}
+
+	$scope.addClassVar = function(classIndex){
+		var newVarType = $("#newVarType_"+classIndex);
+		var newVarName = $("#newVarName_"+classIndex);
+		var newVarValue = $("#newVarValue_"+classIndex);
+
+		$scope.objects[classIndex].vars.push({"type":newVarType.val(),"name":newVarName.val(),"value":newVarValue.val()});
+		newVarType.val('');
+		newVarName.val('');
+		newVarValue.val('');
+	}
 
 
 });
