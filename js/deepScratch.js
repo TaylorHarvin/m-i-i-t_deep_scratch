38d6b7a2df8e1239eeb58d4fb6d4a0f1a9c0b587
Taylor Harvin <tharvin12@gmail.com>
2017-02-24 20:23:57 -0600

    Added template for object -- index

--- js/deepScratch.js
+++ js/deepScratch.js
@@ -2,8 +2,7 @@ var deepScratchApp = angular.module('deepScratchApp',[]);
 
 
 deepScratchApp.controller("objectCreator",function($scope, $rootScope,$http){
-
+	$scope.objects = [{"vars":{"type":"int","name":"width","value":"5"}}];
 
 
 });
-
